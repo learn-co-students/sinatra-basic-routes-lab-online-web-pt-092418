2ec48843911abd17c49c738a5b4e69ef671caba9
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-01-07 20:57:24 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Codegurl"
+  end
+  get '/hometown' do
+    "My hometown is browserville"
+  end
+  get '/favorite-song' do
+    "My favorite song is Ruby"
+  end
 end
