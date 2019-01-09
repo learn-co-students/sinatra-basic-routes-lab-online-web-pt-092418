568c6fb94ad32651e9ad2adf32c366dfc0525f2d
Karyme Virginia <karyme@kv.codes>
2019-01-09 10:57:16 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Karyme"
+  end
+
+  get '/hometown' do
+    "My hometown is Hagerstown"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is some kind of Chillstep"
+  end
+  
 end
