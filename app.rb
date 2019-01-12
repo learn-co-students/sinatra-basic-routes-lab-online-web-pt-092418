d85571159094077265390de4fcbcfce440ba0440
Patrick Shim <patrickshim@Patricks-MBP.home>
2019-01-12 09:26:08 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Patrick Shim"
+  end
+
+  get '/hometown' do
+    "My hometown is Potomac, Maryland"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Bird On a Wire"
+  end
 end
